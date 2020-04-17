f5cbd680d517aefdd92f28a0bbb1a6252308ab61
cbanerjee <63794529+cbanerjee@users.noreply.github.com>
2020-04-17 23:51:32 +0530

    Number of lines reduced
    Comment Lines reduced

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 #File= guessinggame.sh
-#This program contains one function, one loop and one if statement as per the requirements
 function findfiles {
 	fileshere=$(find -mindepth 1 -maxdepth 1 -printf a | wc -c ) 
 #	((fileshere--))
 }
-#This was the function
 findfiles
 echo "Guess the number of files in the working directory"
 read userinput
@@ -17,14 +15,12 @@ done
 userinput=$(echo $((10#$userinput)))
 while [ $fileshere != $userinput ]
 do
-#Here goes the loop
 	if [[ -n ${userinput//[0-9]/} ]] || [[ $userinput -lt 0 ]] || [[ $userinput -gt 9223372036854775807 ]]
 	then
    		echo "Enter a valid number"
 	else
 		if [[ $fileshere -gt $userinput ]]
 		then
-		#Here goes the if statement
 			echo "You entered $userinput and that is LESS than the actual number"
 		else
 			echo "You entered $userinput and that is GREATER than the actual number"
@@ -40,4 +36,3 @@ do
 	userinput=$(echo $((10#$userinput)))
 done
 echo "Congratulations! You have entered the correct number of files, here are $fileshere number of files in this directory"
-#Here is complete number of lines between 20 and 50
